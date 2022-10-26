9cd07096916f1b4a2b4176d790b6e012474b23f5
Kwadwo-prince <asamoahprince001@gmail.com>
2022-10-25 22:32:02 -0700

    The numbers speak for themselves

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,7 +2,7 @@
 
 /**
  * print_numbers - print num from 0 - 9
- * Return
+ * Return: void
  */
 void print_numbers(void)
 {
@@ -10,7 +10,7 @@ void print_numbers(void)
 
 	for (c = '0'; c <= '9'; c++)
 	{
-		_putchar(c)
+		_putchar (c);
 	}
 	_putchar ('\n');
 }
